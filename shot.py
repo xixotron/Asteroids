15c3d05e6c2a4b70655c19b68faa6cf2b45d9d49
xixotron <xixotron89@gmail.com>
2025-07-19 22:39:58 +0200

    shots wrap and have a limited range/lifetime

--- shot.py
+++ shot.py
@@ -1,15 +1,19 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import SHOT_RADIUS
+from constants import SHOT_RADIUS, SHOT_DURATION
 
 class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
+        self.duration = SHOT_DURATION
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
 
     def update(self, dt):
-        self.position += self.velocity * dt
-
+        if self.duration < 0:
+            self.kill()
+            return
+        self.duration -= dt
+        super().update(dt)
