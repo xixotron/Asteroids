56e4f45948a7e57b1c56930f8fe9465654416edd
xixotron <xixotron89@gmail.com>
2025-07-19 16:06:38 +0200

    Now you can close the window!

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
 
