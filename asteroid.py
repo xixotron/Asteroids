900ebf8f7e9fe0d468d3d09ec56b2e893336f1d9
xixotron <xixotron89@gmail.com>
2025-07-19 22:09:56 +0200

    asteroids wrap arround the screen

--- asteroid.py
+++ asteroid.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 from circleshape import CircleShape
-from constants import ASTEROID_MIN_RADIUS
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -13,6 +13,16 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+        if self.position.x >= SCREEN_WIDTH + self.radius:
+            self.position.x -= SCREEN_WIDTH + self.radius * 2
+        elif self.position.x <= 0 - self.radius:
+            self.position.x += SCREEN_WIDTH + self.radius * 2
+
+        if self.position.y >= SCREEN_HEIGHT + self.radius:
+            self.position.y -= SCREEN_HEIGHT + self.radius * 2
+        elif self.position.y <= 0 - self.radius:
+            self.position.y += SCREEN_HEIGHT + self.radius * 2
 
     def split(self):
         self.kill()
