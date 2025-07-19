eaa056a6a5a6d5bfc8ecc0fa46014b46be6662fd
xixotron <xixotron89@gmail.com>
2025-07-19 16:12:31 +0200

    cleanup of comments and prints

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 
 from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
 
