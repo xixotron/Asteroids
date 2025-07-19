5f85056b9bb254c0bc6b4cb780107c29f3e620e3
xixotron <xixotron89@gmail.com>
2025-07-19 16:04:48 +0200

    Start displaying something

--- main.py
+++ main.py
@@ -3,8 +3,17 @@
 # throughout this file
 import pygame
 
+from constants import *
+
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
