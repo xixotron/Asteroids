a9fd95d58ce1a11740a4a7598dd5fead34864f18
xixotron <xixotron89@gmail.com>
2025-07-19 16:42:56 +0200

    Player is in the game!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+from player import Player
 from constants import *
 
 def main():
@@ -8,13 +9,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
